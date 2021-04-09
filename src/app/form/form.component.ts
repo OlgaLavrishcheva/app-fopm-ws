43fc83e178dad7a7830b4eeaa268a2a1ad9a8f3c
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-04-09 17:41:38 +0300

    простая форма

---
+++ src/app/form/form.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import {User} from '../user.class';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.scss']
+})
+export class FormComponent implements OnInit {
+
+  roles: string[] = ['Гость', 'Модератор', 'Администратор'];
+  model: User = new User(0, '', '', 0);
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  get diagnostic(): string {
+    return JSON.stringify(this.model);
+}
+}
